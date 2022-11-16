f19b9a27715cc65d17392be587f8cc84dbe1ad2f
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2022-11-16 21:07:13 +0500

    user  control

---
+++ UserControls/UserControl1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace UserControls
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbState = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbState
+            // 
+            this.cmbState.FormattingEnabled = true;
+            this.cmbState.Location = new System.Drawing.Point(72, 19);
+            this.cmbState.Name = "cmbState";
+            this.cmbState.Size = new System.Drawing.Size(151, 28);
+            this.cmbState.TabIndex = 0;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cmbState);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComboBox cmbState;
+    }
+}
---
+++ UserControls/UserControl1.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace UserControls
+{
+    public partial class UserControl1 : UserControl
+    {
+
+        public string SelectedText
+        {
+            get { return cmbState.Text; }
+        }
+        public string SelectedValue
+        {
+            get { return cmbState.SelectedValue.ToString(); }
+        }
+        public event EventHandler SelectedIdexChanged;
+        public UserControl1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(UserControl1_load);
+            this.cmbState.SelectedIndexChanged += new EventHandler(cmbState_SelectedIndexChanged);
+        }
+
+        void cmbState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(SelectedIdexChanged != null)
+                SelectedIdexChanged(sender, e);
+        }
+        void UserControl1_load(object sender, EventArgs e)
+        {
+            BlindComboBox();
+        }
+        private void BlindComboBox()
+        {
+            DataTable dtState = new System.Data.DataTable();
+            dtState.Columns.Add("txtPart");
+            dtState.Columns.Add("valuePart");
+            dtState.Rows.Add("Chishtian", "1");//Here Delhi is txtPart and 1 is valuepart  
+            dtState.Rows.Add("Faisalabab", "2");
+            dtState.Rows.Add("Punjab", "3");
+            dtState.Rows.Add("Sindh", "4");
+            cmbState.DataSource = dtState;
+            cmbState.DisplayMember = "txtPart";
+            cmbState.ValueMember = "ValuePart";
+
+        }
+    }
+}
